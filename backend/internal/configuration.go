d6008721bd9a130cd632cd4021dddbb43ebdc151
Andrew Tran <andrew.tran345@gmail.com>
2018-12-30 14:01:33 -0800

    added yaml parsing

--- backend/internal/configuration.go
+++ backend/internal/configuration.go
@@ -1,11 +1,37 @@
 package configuration
 
-type Constants struct {
-	PORT     string
+import (
+	"io/ioutil"
+	"log"
+
+	"gopkg.in/yaml.v2"
+)
+
+// Configuration -- Structure that contains mainly database configuration details.
+type Configuration struct {
+	Port     string `yaml:"Port"`
 	Postgres struct {
-		url      string
-		dbname   string
-		username string
-		password string
+		URL      string `yaml:"url"`
+		Dbname   string `yaml:"dbname"`
+		Username string `yaml:"username"`
+		Password string `yaml:"password"`
 	}
+}
+
+const configurationFilename = "configuration.yaml"
+
+// New -- Construct a new Configuration instance and returns it.
+func New() *Configuration {
+	configuration := Configuration{}
+	configurationFile, err := ioutil.ReadFile(configurationFilename)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = yaml.Unmarshal(configurationFile, configuration)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return &configuration
 }
