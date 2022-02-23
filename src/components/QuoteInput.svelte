0da08aac5f7fc7f1ac4bfc2c3a4403c387612ae6
andrewtran1995 <andrew.tran345@gmail.com>
2022-02-23 00:43:48 -0800

    Remove tailwindcss-debug-screens for production

--- src/components/QuoteInput.svelte
+++ src/components/QuoteInput.svelte
@@ -9,6 +9,6 @@
 </script>
 
 <form class="flex justify-center justify-between md:flex-row flex-col space-y-2 md:space-y-0 !mb-4 w-full" on:submit|preventDefault={addQuote}>
-    <input class="bg-green-200 focus:outline-none focus:border-2 focus:border-emerald-600 flex-grow md:mr-4 p-2 rounded-lg transition-colors" bind:value={quoteInput} required>
+    <input class="bg-green-200 focus:outline-none focus:border-2 focus:border-emerald-600 flex-grow md:mr-4 p-2 rounded-lg transition-colors" bind:value={quoteInput} placeholder="Enter a quote" required>
     <button class="border-2 border-emerald-600 p-2 hover:bg-emerald-600 hover:text-white rounded-lg transition-colors">Add quote</button>
 </form>
--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,7 +1,8 @@
 <script>
   import '../app.css'
+  import { dev } from '$app/env'
 </script>
 
-<div class="bg-gray-100 h-screen">
+<div class="bg-gray-100 h-screen {dev && 'debug-screens'}">
     <slot/>
 </div>
